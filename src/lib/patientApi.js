3aa33c6081bf7ea14c81a07cec0541f7cb064cb9
Zekerie <scade330@gmail.com>
2025-12-02 16:42:42 +0300

    f

--- src/lib/patientApi.js
+++ src/lib/patientApi.js
@@ -1,30 +1,47 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_BASE_URL + "/api/patients";
+// Ensure trailing slash is correct
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const API = `${BASE_URL}/api/patients`;
 
 // CREATE
-export const createPatient = (data) => axios.post(`${API_URL}/create`, data, { withCredentials: true });
+export const createPatient = async (data) => {
+  const res = await axios.post(`${API}/create`, data, {
+    withCredentials: true,
+  });
+  return res.data;
+};
 
 // READ ALL
 export const getAllPatients = async () => {
-  const res = await axios.get(`${API_URL}/all`, { withCredentials: true });
-  return res.data; // array of patients
+  const res = await axios.get(`${API}/all`, {
+    withCredentials: true,
+  });
+
+  // Backend returns { patients: [...] }
+  return res.data.patients; // ✅ return clean array
 };
 
 // READ ONE
 export const getPatientById = async (id) => {
-  const res = await axios.get(`${API_URL}/id/${id}`, { withCredentials: true });
+  const res = await axios.get(`${API}/id/${id}`, {
+    withCredentials: true,
+  });
   return res.data;
 };
 
 // UPDATE
 export const updatePatient = async (id, data) => {
-  const res = await axios.put(`${API_URL}/id/${id}`, data, { withCredentials: true });
+  const res = await axios.put(`${API}/id/${id}`, data, {
+    withCredentials: true,
+  });
   return res.data;
 };
 
 // DELETE
 export const deletePatient = async (id) => {
-  const res = await axios.delete(`${API_URL}/id/${id}`, { withCredentials: true });
+  const res = await axios.delete(`${API}/id/${id}`, {
+    withCredentials: true,
+  });
   return res.data;
 };
