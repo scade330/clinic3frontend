3603f2dec5e03018b7bbf86c1f34168a40dc427a
Zekerie <scade330@gmail.com>
2026-01-02 16:12:47 +0300

    Initial commit

--- src/lib/userApi.js
+++ src/lib/userApi.js
@@ -1,12 +1,17 @@
-// src/api/userApi.js
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL.replace(/\/$/, ""); // remove trailing slash
+// Remove trailing slash if present
+const BASE_URL = import.meta.env.VITE_API_URL.replace(/\/$/, "");
 const API = `${BASE_URL}/api/user`;
 
 export const loginUser = async (data) => {
   try {
-    const res = await axios.post(`${API}/login-user`, data, { withCredentials: true });
+    const res = await axios.post(`${API}/login-user`, data, {
+      withCredentials: true, // only if your backend sets cookies
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     return res.data.user || res.data;
   } catch (error) {
     console.error("Login error:", error.response || error);
