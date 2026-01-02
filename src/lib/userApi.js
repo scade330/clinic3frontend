6b7da6b3e9e62fd6d91208952e262a92b0ea723c
Zekerie <scade330@gmail.com>
2026-01-02 16:43:31 +0300

    Initial commit

--- src/lib/userApi.js
+++ src/lib/userApi.js
@@ -4,14 +4,19 @@ import axios from "axios";
 const BASE_URL = import.meta.env.VITE_API_URL.replace(/\/$/, "");
 const API = `${BASE_URL}/api/user`;
 
+// Axios instance configured for credentials
+const apiClient = axios.create({
+  baseURL: API,
+  withCredentials: true, // allow cookies to be sent/received
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Login user
 export const loginUser = async (data) => {
   try {
-    const res = await axios.post(`${API}/login-user`, data, {
-      withCredentials: true, // only if your backend sets cookies
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const res = await apiClient.post("/login-user", data);
     return res.data.user || res.data;
   } catch (error) {
     console.error("Login error:", error.response || error);
